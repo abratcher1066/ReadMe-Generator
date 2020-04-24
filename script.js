df42c4277428d3d9650acba68d4ceade401f9356
tanksareforcowards <37027181+tanksareforcowards@users.noreply.github.com>
2020-04-23 23:45:16 -0400

    got the basics of js index set up and added starter files

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Readme Generator
+// First we get fs
+const fs = require('fs');
+
+
+// Setting up dependencies and stuff
+// we're going to use inquirer per gary's instructions to ask the user for info
+// to generate their readme.
+
+const inquierer = require('inquirer');
+
+
+// let's write to a test file until other features are working/work is donwe
+
+fs.writeFile("log.txt", msg, function(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("Success!")
+});
